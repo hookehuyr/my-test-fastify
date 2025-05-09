498ad07cb0f42826a5640fb91c63ca83786fead6
hookehuyr <hookehuyr@gmail.com>
2025-05-09 14:09:00 +0800

    feat(生命周期钩子): 在Fastify中添加多个生命周期钩子
    在app.js中添加了preParsing、preValidation、preHandler、preSerialization、onError、onSend、onResponse和onTimeout钩子，以增强请求处理流程的灵活性和控制力。同时，新增了授权-生命周期.md文档，详细介绍了在Fastify中实现JWT授权校验时各生命周期钩子的适用场景和最佳实践。

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-05-04 22:32:34
  * @LastEditors: hookehuyr hookehuyr@gmail.com
- * @LastEditTime: 2025-05-08 21:24:27
+ * @LastEditTime: 2025-05-08 23:42:32
  * @FilePath: /my-test-fastify/app.js
  * @Description: Fastify应用程序入口文件
  */
@@ -19,8 +19,55 @@ module.exports = async function (fastify, opts) {
   // 在这里放置你的自定义代码！
   fastify.addHook('onRequest', async (request, reply) => {
     // Some code
-    request.log.info('±onRequest±')
+    request.log.info('onRequest')
+    // reply.code(400)
+    // throw new Error('Some error')
+    return reply
   })
+
+  fastify.addHook('preParsing', async (request, reply, payload) => {
+    // 其他代码
+    request.log.info('preParsing');
+    return payload
+  })
+  // 校验前处理
+  fastify.addHook('preValidation', async (request, reply) => {
+    request.log.info('preValidation');
+    // const importantKey = await generateRandomString()
+    request.body = { ...request.body, importantKey: '123456' }
+  })
+  // 校验后处理, user的Handler之前
+  fastify.addHook('preHandler', async (request, reply) => {
+    // 其他代码
+    request.log.info('preHandler');
+    return reply
+  })
+  // 序列化前处理, 也就是发送到客户端之前
+  fastify.addHook('preSerialization', async (request, reply, payload) => {
+    request.log.info('preSerialization');
+    // payload { message: '登录成功' }
+    // 无论成功失败, 都会触发
+    // 在这里是全局修改了, 所有的输出都带上了 ret: 'OK', msg: '请求成功'
+    return payload
+  })
+  // onError 钩子可用于自定义错误日志，或当发生错误时添加特定的 header
+  fastify.addHook('onError', async (request, reply, error) => {
+    // 当自定义错误日志时有用处
+    // 你不应该使用这个钩子去更新错误
+  })
+  // 使用 onSend 钩子可以改变 payload
+  fastify.addHook('onSend', async (request, reply, payload) => {
+    return payload
+  })
+  //
+  fastify.addHook('onResponse', async (request, reply) => {
+    // 其他代码
+  })
+  // 监测请求超时
+  fastify.addHook('onTimeout', async (request, reply) => {
+    // 其他代码
+  })
+
 
   // 自定义一个可共享的JSON模式
   const customSchema = {
