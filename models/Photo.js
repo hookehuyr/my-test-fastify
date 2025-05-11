505efa4a0fb55ae3280115e4bb2aea1df5327cd5
hookehuyr <hookehuyr@gmail.com>
2025-05-12 00:17:58 +0800

    feat(photo): 添加照片相关查询接口
    添加了获取照片列表、模糊查询照片、根据条件查询照片列表及获取照片数量和列表的接口。这些接口提高了照片管理的灵活性和功能性。

--- models/Photo.js
+++ models/Photo.js
@@ -1,32 +1,59 @@
 /*
  * @Date: 2025-05-11 22:14:41
  * @LastEditors: hookehuyr hookehuyr@gmail.com
- * @LastEditTime: 2025-05-11 22:21:31
+ * @LastEditTime: 2025-05-12 00:10:16
  * @FilePath: /my-test-fastify/models/Photo.js
  * @Description: 文件描述
  */
 'use strict'
 
+const { Like } = require("typeorm")
+
 class Photo {
-    constructor(fastify) {
-        this.fastify = fastify
-        this.photoRepository = fastify.orm.getRepository('Photo')
+  constructor(fastify) {
+    this.fastify = fastify
+    this.photoRepository = fastify.orm.getRepository('Photo')
+  }
+
+  async create(photoData) {
+    const { name, description, filename, views, isPublished } = photoData
+
+    const photo = this.photoRepository.create({
+      name,
+      description,
+      filename,
+      views,
+      isPublished
+    })
+
+    await this.photoRepository.save(photo)
+    return photo
+  }
+
+  async findAll() {
+    return await this.photoRepository.find()
+  }
+
+  async findOneBy(query) {
+    // 如果是通过name查询，使用Like进行模糊匹配
+    if (query.name) {
+      return await this.photoRepository.findOne({
+        where: {
+          name: Like(`%${query.name}%`)
+        }
+      })
     }
+    // 其他查询条件保持原样
+    return await this.photoRepository.findOneBy(query)
+  }
 
-    async create(photoData) {
-        const { name, description, filename, views, isPublished } = photoData
+  async findBy(query) {
+    return await this.photoRepository.findBy(query)
+  }
 
-        const photo = this.photoRepository.create({
-            name,
-            description,
-            filename,
-            views,
-            isPublished
-        })
-
-        await this.photoRepository.save(photo)
-        return photo
-    }
+  async findAndCount() {
+    return await this.photoRepository.findAndCount()
+  }
 }
 
 module.exports = Photo
--- routes/photo/index.js
+++ routes/photo/index.js
@@ -44,4 +44,127 @@ module.exports = async function (fastify, opts) {
       })
     }
   })
+
+  fastify.get('/list', {
+    schema: {
+      tags: ['photo'],
+      summary: '获取照片列表',
+      description: '获取照片列表',
+    }
+  }, async function (request, reply) {
+    try {
+      const photoModel = new Photo(fastify)
+      const photos = await photoModel.findAll()
+      return reply.send({
+        message: '获取成功',
+        data: photos
+      })
+    } catch (error) {
+      return reply.status(500).send({
+        message: '获取失败',
+        error: error.message
+      })
+    }
+  })
+
+  fastify.get('/getPhotoBy', {
+    schema: {
+      tags: ['photo'],
+      summary: '获取照片',
+      description: '获取照片',
+      querystring: {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+          name: { type: 'string', description: '照片名称（支持模糊查询）' }
+        },
+        oneOf: [
+          { required: ['id'] },
+          { required: ['name'] }
+        ]
+      }
+    }
+  }, async function (request, reply) {
+    try {
+      const query = request.query
+      const photoModel = new Photo(fastify)
+      let photo = null
+      if (query) {
+        photo = await photoModel.findOneBy(query)
+      }
+      return reply.send({
+        message: '获取成功',
+        data: photo
+      })
+    } catch (error) {
+      return reply.status(500).send({
+        message: '获取失败',
+        error: error.message
+      })
+    }
+  })
+
+  fastify.get('/getPhotosBy', {
+    schema: {
+      tags: ['photo'],
+      summary: '获取照片列表',
+      description: '获取照片列表',
+      querystring: {
+        type: 'object',
+        properties: {
+          views: { type: 'number' },
+          isPublished: { type: 'boolean' }
+        },
+        oneOf: [
+          { required: ['views'] },
+          { required: ['isPublished'] }
+        ]
+      }
+    }
+  }, async function (request, reply) {
+    try {
+      const query = request.query
+      const photoModel = new Photo(fastify)
+      let photos = null
+      if (query) {
+        photos = await photoModel.findBy(query)
+      }
+      return reply.send({
+        message: '获取成功',
+        data: photos
+      })
+    } catch (error) {
+      return reply.status(500).send({
+        message: '获取失败',
+        error: error.message
+      })
+    }
+  })
+
+  fastify.get('/getPhotoAndCount', {
+    schema: {
+      tags: ['photo'],
+      summary: '获取照片列表和数量',
+      description: '获取照片列表和数量',
+    }
+  }, async function (request, reply) {
+    try {
+      const photoModel = new Photo(fastify)
+      const [photos, count] = await photoModel.findAndCount()
+      return reply.send({
+        message: '获取成功',
+        data: {
+          photos,
+          count
+        }
+      })
+    } catch (error) {
+      return reply.status(500).send({
+        message: '获取失败',
+        error: error.message
+      })
+    }
+  })
+
+
 }
